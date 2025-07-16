1a851fee59a9b1baf420f38b5975143380ac6560
MIshata <nasko65@abv.bg>
2025-07-16 17:15:05 +0300

    Add files via upload

---
+++ C#_Small Projects/#2/Second Projects/Program.cs
@@ -0,0 +1,19 @@
+﻿var bitcoin = int.Parse(Console.ReadLine());
+var ioan = double.Parse(Console.ReadLine());
+var comission = double.Parse(Console.ReadLine());
+
+b * 1168 i * 0.15 * 1.76 b + i / 1.95
+ x - x * 0.05
+
+var levab = bitcoin * 1168;
+var levai = ioan * 0.15 * 1.76;
+var euro = (levab + levai) / 1.95;
+var result = euro - euro * comission / 100;
+
+Console.WriteLine("Result:" + result + "Euro");
+
+int btc = int.Parse(Console.ReadLine());
+double yoan = double.Parse(Console.ReadLine());
+double comission = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Result: {(100 - comission) / 100 * ((btc * 1168 + yoan * 0.15 * 1.76) / 1.95)} euro");
