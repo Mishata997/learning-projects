fa3624f810f120cba3ee306e66acbb8236049a7a
MIshata <nasko65@abv.bg>
2025-07-18 10:32:52 +0300

    Add files via upload

---
+++ C#_Small Projects/#4/4 App/Program.cs
@@ -0,0 +1,24 @@
+﻿var n = int.Parse(Console.ReadLine());
+
+var sumLeft = 0;
+for (int i = 1; i <= n; i++)
+{
+    var number = int.Parse(Console.ReadLine());
+    sumLeft += number;
+}
+
+var sumRight = 0;
+for (int i = 1; i <= n; i++)
+{
+    var number = int.Parse(Console.ReadLine());
+    sumRight += number;
+}
+var sum = sumLeft - sumRight;
+
+if (sumLeft == sumRight)
+{ Console.WriteLine("Yes, sum = " + sumLeft); }
+else
+{
+    Console.WriteLine("No, diff = " +
+Math.Abs(sumRight - sumLeft));
+}
