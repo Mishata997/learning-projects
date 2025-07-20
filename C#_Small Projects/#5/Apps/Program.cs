a4c5c2402810496de1041ea574e95672952c4a16
MIshata <nasko65@abv.bg>
2025-07-20 17:06:54 +0300

    Add files via upload

---
+++ C#_Small Projects/#5/Apps/Program.cs
@@ -0,0 +1,17 @@
+﻿var Общият_бюджет = double.Parse(Console.ReadLine());
+var Цената_на_книгите = double.Parse(Console.ReadLine());
+
+var дрехи = Цената_на_книгите * 0.2;
+var козметика = Цената_на_книгите * 0.05;
+var разходи = Цената_на_книгите * 0.12;
+var харчи = Цената_на_книгите + дрехи + козметика + разходи;
+var Остават = Math.Round(Общият_бюджет - харчи, 2);
+
+if (Остават >= 0)
+{
+    Console.WriteLine($"Yes! {Остават} leva left.");
+}
+else
+{
+    Console.WriteLine($"No! {Math.Abs(Остават)} leva needed.");
+}
